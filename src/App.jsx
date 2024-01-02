93b281c395e91fffcb9c919ebf068da258c6b2e7
Billy Ti <78161308+Billy-Ti@users.noreply.github.com>
2024-01-02 15:00:06 +0800

    Update ⬆️ Adjust the annotation usage instructions

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Form from "./Form";
 const App = () => {
   // const handleRender = (
   //   id, // 傳給 Profiler 的 id，此範例是 <Form /> component
-  //   phase, // "mount"（第一次渲染）|| "update"（重新渲染 re-render）|| "unmount" (元件卸載)
-  //   actualDuration, // 元件渲染所需的時間，優化時可特別注意這個
-  //   baseDuration, // 不使用優化 (memoization) 的情况下，此元件渲染的時間
+  //   phase, // "mount"（第一次渲染）|| "update"（重新渲染 re-render）|| "unmount" (組件卸載)
+  //   actualDuration, // 組件渲染所需的時間，優化時可特別注意這個
+  //   baseDuration, // 不使用優化 (memoization) 的情况下，此組件渲染的時間
   //   startTime, // 本次更新中 React 開始渲染的時間點
-  //   commitTime, // 本次更新中 React commit 的時間點
+  //   commitTime, // 本次更新中 React commit 結束後的時間點
   // ) => {
-  //   // 輸出性能資訊或處理性能資料
+  //   // 輸出效能資訊或處理效能資料
   //   console.log(`Profiler-ID: ${id}`);
   //   console.log(`Render-phase: ${phase} (狀態)`);
   //   console.log(`Actual-duration: ${actualDuration.toFixed(2)} (毫秒)`);
